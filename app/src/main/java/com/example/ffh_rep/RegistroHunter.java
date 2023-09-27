9a9ef50f0a12a555400acfe769274548a0905c19
lucas <lucgom99@gmail.com>
2023-09-26 23:09:23 -0300

    activity reguster hunter y comercio con sus layout

---
+++ app/src/main/java/com/example/ffh_rep/RegistroHunter.java
@@ -0,0 +1,14 @@
+package com.example.ffh_rep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistroHunter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_hunter);
+    }
+}
