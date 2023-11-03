55b6180d8ca735f38c702ec34968772ab5831ebd
Pablo Ezequiel Caero <pablo_ieme@hotmail.com>
2023-11-03 01:11:20 -0300

    Cambios esteticos

--- app/src/main/java/com/example/ffh_rep/ui/commerce/Comercio_Ver_Articulo.java
+++ app/src/main/java/com/example/ffh_rep/ui/commerce/Comercio_Ver_Articulo.java
@@ -25,6 +25,7 @@ import com.example.ffh_rep.databinding.FragmentComercioVerArticuloBinding;
 import com.example.ffh_rep.entidades.Articulo;
 import com.example.ffh_rep.entidades.Comercio;
 import com.example.ffh_rep.entidades.ItemCarrito;
+import com.example.ffh_rep.entidades.Stock;
 import com.example.ffh_rep.factory.ArticuloViewModelFactory;
 import com.example.ffh_rep.factory.CarritoViewModelFactory;
 import com.example.ffh_rep.factory.HunterVerComercioViewModelFactory;
@@ -39,11 +40,9 @@ public class Comercio_Ver_Articulo extends Fragment {
     private CarritoViewModel carrito;
     private Articulo article;
     private FragmentComercioVerArticuloBinding binding;
-    private TextView descripcion, precio, categoria, marca, cantidadArticulo;
+    private TextView descripcion, precio, categoria, marca;
     private ImageView ivArticulo;
-    private Button btnAniadirCarrito, btnSumar, btnRestar;
-    private ItemCarrito item;
-    private int _cantidadArticulo;
+
     public static com.example.ffh_rep.ui.hunter.Hunter_VerArticulo newInstance() {
         return new com.example.ffh_rep.ui.hunter.Hunter_VerArticulo();
     }
@@ -55,8 +54,6 @@ public class Comercio_Ver_Articulo extends Fragment {
         binding  = FragmentComercioVerArticuloBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
-        //LA CANTIDAD DEL ARTICULO DEBERIA SER EL STOCK DISPONIBLE
-        _cantidadArticulo = 0;
 
         initComponentes(view);
 
@@ -71,29 +68,19 @@ public class Comercio_Ver_Articulo extends Fragment {
         setUpListeners(article);
 
 
-        carrito.getCarrito().observe(getViewLifecycleOwner(), new Observer<List<ItemCarrito>>() {
-            @Override
-            public void onChanged(List<ItemCarrito> itemCarritos) {
-                Log.d("CARRITO EN DEATLLE ARTICULO", itemCarritos.toString());
-            }
-        });
-
         return view;
     }
 
+
     public void initComponentes(View view){
-        carrito = new ViewModelProvider(requireActivity(), new CarritoViewModelFactory(getActivity())).get(CarritoViewModel.class);
         controller_articulos = new ViewModelProvider(this, new ArticuloViewModelFactory(getActivity())).get(ArticulosViewModel.class);
 
         descripcion = view.findViewById(R.id.tvDescripcionArt);
         precio = view.findViewById(R.id.tvPrecioArt);
         marca = view.findViewById(R.id.tvMarcaArt);
         categoria = view.findViewById(R.id.tvCategoriaArt);
-        cantidadArticulo = view.findViewById(R.id.tv_cantidadItem);
-        btnAniadirCarrito = view.findViewById(R.id.btnAgregarCarrito);
         ivArticulo = view.findViewById(R.id.ivArticulo);
-        btnSumar = view.findViewById(R.id.btnSumarItem);
-        btnRestar = view.findViewById(R.id.btnRestarItem);
+
     }
 
     public void settingComponents(Articulo article, View view){
@@ -105,58 +92,17 @@ public class Comercio_Ver_Articulo extends Fragment {
     }
 
     public void setUpListeners(Articulo article){
-        btnAniadirCarrito.setOnClickListener(v -> addArticle(article));
-        btnSumar.setOnClickListener(v -> sumarItem());
-        btnRestar.setOnClickListener(v -> restarItem());
+
     }
 
-    public void addArticle(Articulo article){
-        if(this._cantidadArticulo > 0){
-            item = new ItemCarrito(article, this._cantidadArticulo);
-            if(!carrito.isArticleInCart(item)){
-                carrito.addArticleToCart(item);
-                cantidadArticulo.setText(String.valueOf(0));
-                Toast.makeText(getContext(), "Articulo Agregado", Toast.LENGTH_LONG).show();
-                this._cantidadArticulo=0;
-            }
-            else{
-                showDuplicateItemDialog();
-            }
 
-        }
-    }
 
-    public void sumarItem(){
-        this._cantidadArticulo+=1;
-        cantidadArticulo.setText(String.valueOf(this._cantidadArticulo));
-    }
 
-    public void restarItem(){
-        if(this._cantidadArticulo > 0){
-            this._cantidadArticulo--;
-            cantidadArticulo.setText(String.valueOf(this._cantidadArticulo));
-        }
-    }
 
-    public void showDuplicateItemDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
-        builder.setTitle("Artículo duplicado");
-        builder.setMessage("Este artículo ya está en el carrito. ¿Deseas agregar más unidades?");
-        builder.setPositiveButton("Sí", (dialog, which) -> {
-            addQuantityToChart();
-            dialog.dismiss();
-        });
-        builder.setNegativeButton("No", (dialog, which) -> {
-            dialog.dismiss();
-        });
-        builder.create().show();
-    }
 
-    public void addQuantityToChart(){
-        carrito.addMoreUnitsToCart(this.item);
-        this._cantidadArticulo=0;
-        cantidadArticulo.setText(String.valueOf(0));
-        Toast.makeText(getContext(), "Se han añadido mas unidades al carrito", Toast.LENGTH_LONG).show();
-    }
+
+
+
+
 
 }
