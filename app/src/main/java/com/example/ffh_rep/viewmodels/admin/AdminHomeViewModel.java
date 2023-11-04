19e2723339411c15c4c4e8119a9cc44bc6288007
juankadev <54191222+Juankadev@users.noreply.github.com>
2023-11-04 01:15:02 -0300

    Admin - Home Estadisticas

--- app/src/main/java/com/example/ffh_rep/viewmodels/admin/AdminHomeViewModel.java
+++ app/src/main/java/com/example/ffh_rep/viewmodels/admin/AdminHomeViewModel.java
@@ -1,7 +1,24 @@
 package com.example.ffh_rep.viewmodels.admin;
 
+import android.content.Context;
+
 import androidx.lifecycle.ViewModel;
 
+import com.example.ffh_rep.models.repositories.AdminRepository;
+import com.example.ffh_rep.models.repositories.ComercioRepository;
+
+import java.util.concurrent.CompletableFuture;
+
 public class AdminHomeViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private Context ctx;
+    private AdminRepository adminRepo;
+    public AdminHomeViewModel(){}
+    public Integer cantidadComercios;
+
+    public AdminHomeViewModel(Context ctx) {
+        this.ctx = ctx;
+        this.adminRepo = new AdminRepository();
+    }
+    public CompletableFuture<Integer> getCantidadComercios(){ return adminRepo.getCantidadComercios(); }
+    public CompletableFuture<Integer> getCantidadHunters(){ return adminRepo.getCantidadHunters(); }
 }
